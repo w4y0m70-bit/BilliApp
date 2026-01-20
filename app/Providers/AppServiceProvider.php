3a9e53a974773945109b93133c6f4af7737a784e
わよう <w4y0m70@gmail.com>
2026-01-20 16:06:03 +0900

    AppServiceProvider.php修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
